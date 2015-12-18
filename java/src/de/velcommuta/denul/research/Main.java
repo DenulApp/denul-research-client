0ac67225a7dc7103ddd5ddf1fa56487f5481bab9
Max Maass <max@velcommuta.de>
2015-12-18 11:06:05 +0100

    Initial commit

---
+++ java/src/de/velcommuta/denul/research/Main.java
@@ -0,0 +1,14 @@
+package de.velcommuta.denul.research;
+
+/**
+ * Main Class
+ */
+public class Main {
+    /**
+     * Main function
+     * @param args Arguments
+     */
+    public static void main(String[] args) {
+        // TODO Do something
+    }
+}
---
+++ java/test/de/velcommuta/denul/research/MainTest.java
@@ -0,0 +1,12 @@
+package de.velcommuta.denul.research;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test class for Main class
+ */
+public class MainTest {
+    // TODO Test something
+}

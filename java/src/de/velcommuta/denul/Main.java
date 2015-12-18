cd2dccc6d9074f10711779f65b740aa53ca3405e
Max Maass <max@velcommuta.de>
2015-12-18 16:04:08 +0100

    Comply with coding conventions

--- java/src/de/velcommuta/denul/Main.java
+++ java/src/de/velcommuta/denul/Main.java
@@ -12,6 +12,6 @@ public class Main {
      * @param args Arguments
      */
     public static void main(String[] args) {
-        new TextUI().MainMenu();
+        new TextUI().mainMenu();
     }
 }
--- java/src/de/velcommuta/denul/ui/TextUI.java
+++ java/src/de/velcommuta/denul/ui/TextUI.java
@@ -17,49 +17,79 @@ public class TextUI {
      */
     public TextUI() {}
 
+
     /**
      * Open the local database and ask for a password, if needed
      * @return true if the database was successfully opened, false otherwise
      */
-    public boolean OpenDatabase() {
+    public boolean openDatabase() {
         // TODO
         return true;
     }
 
+
     /**
      * Create a new request
      */
-    public void NewRequest() {
+    public void newRequest() {
         // TODO
+        println("");
+        println("All questions are modelled after the medical study information sheet of the");
+        println("Office for the Protection of Research Subjects of the University of Sourthern");
+        println("California (USC), which can be found at");
+        println("http://oprs.usc.edu/files/2013/04/Informed-Consent-Booklet-4.4.13.pdf");
+        println("Please refer to that document to learn more about the individual questions.");
+        println("");
     }
+
+
+    /**
+     * View active studies
+     */
+    public void viewActiveStudies() {
+        println("NotImplemented");
+    }
+
+
+    /**
+     * View settings studies
+     */
+    public void viewSettings() {
+        println("NotImplemented");
+    }
+
 
     /**
      * Show the main menu of the application
      */
-    public void MainMenu() {
+    public void mainMenu() {
         // Define the main menu options
-        String[] menuOptions = {"New research request", "View active research data", "Quit"};
+        String[] menuOptions = {"New research request", "View active research data", "Settings", "Quit"};
         // Print the program header
         println("    Denul Research Client\n");
         // Main loop
-        boolean running = OpenDatabase();
+        boolean running = openDatabase();
         while (running) {
             // Ask for menu selection
             int selection = readSelection("Welcome to the Denul Research Client. Please choose what you want to do:", menuOptions);
             switch (selection) {
                 case 0:
                     // New research request
-                    NewRequest();
+                    newRequest();
                     break;
                 case 1:
-                    // View existing research requests
-                    println("NotImplemented");
+                    // View existing research data
+                    viewActiveStudies();
                     break;
                 case 2:
+                    // View the settings
+                    viewSettings();
+                    break;
+                case 3:
                     running = false;
                     break;
                 default:
-                    println("NotImplemented - wtf?");
+                    println("WARN: " + menuOptions[selection] + " not implemented in switch statement");
             }
         }
     }

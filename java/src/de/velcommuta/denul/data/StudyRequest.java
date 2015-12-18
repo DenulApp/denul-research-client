7ef7ea384c3728e30505c3320c4a31977cbe6e62
Max Maass <max@velcommuta.de>
2015-12-18 16:45:37 +0100

    Add basic data types, data input

---
+++ java/src/de/velcommuta/denul/data/StudyRequest.java
@@ -0,0 +1,95 @@
+package de.velcommuta.denul.data;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Data holder class for study requests
+ */
+public class StudyRequest {
+    public String name;
+    public String institution;
+    public String webpage;
+    public String description;
+    public String purpose;
+    public String procedures;
+    public String risks;
+    public String benefits;
+    public String payment;
+    public String conflicts;
+    public String confidentiality;
+    public String participationAndWithdrawal;
+    public String rights;
+    public List<Investigator> investigators = new LinkedList<>();
+
+    /**
+     * Empty constructor
+     */
+    public StudyRequest() {};
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Name: ");
+        builder.append(name);
+        builder.append("\n");
+
+        builder.append("Institution: ");
+        builder.append(institution);
+        builder.append("\n");
+
+        builder.append("Web page: ");
+        builder.append(webpage);
+        builder.append("\n\n");
+
+        builder.append("Description:\n");
+        builder.append(description);
+        builder.append("\n\n");
+
+        builder.append("Purpose:\n");
+        builder.append(purpose);
+        builder.append("\n\n");
+
+        builder.append("Procedures:\n");
+        builder.append(procedures);
+        builder.append("\n\n");
+
+        builder.append("Risks:\n");
+        builder.append(risks);
+        builder.append("\n\n");
+
+        builder.append("Benefits:\n");
+        builder.append(benefits);
+        builder.append("\n\n");
+
+        builder.append("Payment:\n");
+        builder.append(payment);
+        builder.append("\n\n");
+
+        builder.append("Conflicts of Interest:\n");
+        builder.append(conflicts);
+        builder.append("\n\n");
+
+        builder.append("Confidentiality:\n");
+        builder.append(confidentiality);
+        builder.append("\n\n");
+
+        builder.append("Participation and Withdrawal:\n");
+        builder.append(participationAndWithdrawal);
+        builder.append("\n\n");
+
+        builder.append("Subjects Rights:\n");
+        builder.append(rights);
+        builder.append("\n\n");
+
+        for (int i = 0; i < investigators.size(); i++) {
+            builder.append("Investigator #");
+            builder.append(i+1);
+            builder.append(":\n");
+            builder.append(investigators.get(i).toString());
+            builder.append("\n");
+        }
+
+        return builder.toString();
+    }
+}
--- java/src/de/velcommuta/denul/ui/TextUI.java
+++ java/src/de/velcommuta/denul/ui/TextUI.java
@@ -1,6 +1,12 @@
 package de.velcommuta.denul.ui;
 
 // Static-import a bunch of methods for more concise code
+import de.velcommuta.denul.data.Investigator;
+import de.velcommuta.denul.data.StudyRequest;
+
+import java.util.LinkedList;
+import java.util.List;
+
 import static de.velcommuta.denul.util.Output.println;
 import static de.velcommuta.denul.util.Output.print;
 import static de.velcommuta.denul.util.Input.readLine;
@@ -32,7 +38,7 @@ public class TextUI {
      * Create a new request
      */
     public void newRequest() {
-        // TODO
+        StudyRequest request = new StudyRequest();
         println("");
         println("All questions are modelled after the medical study information sheet of the");
         println("Office for the Protection of Research Subjects of the University of Sourthern");
@@ -40,6 +46,45 @@ public class TextUI {
         println("http://oprs.usc.edu/files/2013/04/Informed-Consent-Booklet-4.4.13.pdf");
         println("Please refer to that document to learn more about the individual questions.");
         println("");
+        request.institution = readLine("Name of your institution");
+        request.name        = readLine("Title of study");
+        request.webpage     = readLine("Web page of study");
+        request.description = readLines("Please give a short description of the study");
+        request.purpose     = readLines("Please explain the purpose of your study");
+        request.procedures  = readLines("Please explain the procedures of this study - what data will be collected, and why");
+        request.risks       = readLines("Please explain any risks associated with this study");
+        request.benefits    = readLines("Please explain the benefits that will come of this study");
+        request.payment     = readLines("Please explain if participants will be paid");
+        request.conflicts   = readLines("Please explain any potential conflicts of interest in this work");
+        request.confidentiality = readLines("Please explain your data security and confidentiality policy");
+        request.participationAndWithdrawal = readLines("Please explain your policy on participating and withdrawing from the study");
+        request.rights      = readLines("Please describe the rights of study participants");
+        println("You will now be asked to add at least one investigator to the study.");
+        println("");
+        request.investigators.addAll(addInvestigators());
+        println("\n\nPlease review the following information:\n");
+        println(request.toString());
+        yes("Is this information correct?");
+    }
+
+
+    /**
+     * Ask the user to input the details of at least one, potentially more, Investigator(s)
+     * @return A List of {@link Investigator}s
+     */
+    public List<Investigator> addInvestigators() {
+        List<Investigator> rv = new LinkedList<>();
+        Investigator investigator = new Investigator();
+        investigator.name = readLine("Please enter the investigators full name");
+        investigator.institution = readLine("Please enter the institution of the investigator");
+        investigator.group = readLine("Please enter the group of the investigator");
+        investigator.position = readLine("Please enter the position of the investigator");
+        rv.add(investigator);
+        if (yes("Do you want to add additional Investigators?")) {
+            println("");
+            rv.addAll(addInvestigators());
+        }
+        return rv;
     }
 
 

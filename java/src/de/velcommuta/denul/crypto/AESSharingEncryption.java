7f44485477b94a780fd64504fc5c88ca6b70a41b
Max Maass <max@velcommuta.de>
2016-01-15 13:16:19 +0100

    Fix AES-GCM vulnerability (see #4)
    This commit, together with commits in other repositories, fixes the
    AES-GCM vulnerability described in #4.  While no explicit unittests
    exist in this repository, they exist in the main Android repo for
    the same code.

--- java/src/de/velcommuta/denul/crypto/AESSharingEncryption.java
+++ java/src/de/velcommuta/denul/crypto/AESSharingEncryption.java
@@ -10,6 +10,7 @@ import de.velcommuta.denul.data.Shareable;
 import de.velcommuta.denul.data.ShareableUnwrapper;
 import de.velcommuta.denul.data.proto.DataContainer;
 
+import java.util.Random;
 import java.util.logging.Logger;
 
 /**
@@ -18,6 +19,7 @@ import java.util.logging.Logger;
 public class AESSharingEncryption implements SharingEncryption {
     private static final Logger logger = Logger.getLogger(AESSharingEncryption.class.getName());
 
+    private static final int IVBYTES = 16;
 
     @Override
     public byte[] encryptKeysAndIdentifier(DataBlock data, KeySet keys) {
@@ -26,10 +28,23 @@ public class AESSharingEncryption implements SharingEncryption {
         byte[] plaintext = new byte[data.getIdentifier().length + data.getKey().length];
         System.arraycopy(data.getIdentifier(), 0, plaintext, 0,                           data.getIdentifier().length);
         System.arraycopy(data.getKey(),        0, plaintext, data.getIdentifier().length, data.getKey().length);
-        // Encrypt identifier-key-pair and return result
+        // Generate a random 128-bit IV component. This is used to prevent an attack based on IV
+        // reuse (see issue #47 in the GitHub repository)
+        byte[] randIV = new byte[IVBYTES];
+        new Random().nextBytes(randIV);
+        byte[] iv = new byte[keys.getOutboundCtr().length + randIV.length];
+        System.arraycopy(keys.getOutboundCtr(), 0, iv, 0,                            keys.getOutboundCtr().length);
+        System.arraycopy(randIV,                0, iv, keys.getOutboundCtr().length, randIV.length);
+        // Encrypt identifier-key-pair
         // The result is bound to the identifier because the identifier is bound to the outbound counter,
         // which is used as IV and thus implicitly authenticated (decryption will fail if it is wrong)
-        return AES.encryptAES(plaintext, keys.getOutboundKey(), null, keys.getOutboundCtr());
+        byte[] encrypted = AES.encryptAES(plaintext, keys.getOutboundKey(), null, iv);
+        if (encrypted == null) return null;
+        // Prepare return value
+        byte[] rv = new byte[randIV.length + encrypted.length];
+        System.arraycopy(randIV,    0, rv, 0,             randIV.length);
+        System.arraycopy(encrypted, 0, rv, randIV.length, encrypted.length);
+        return rv;
     }
 
 
@@ -59,7 +74,17 @@ public class AESSharingEncryption implements SharingEncryption {
     public DataBlock decryptKeysAndIdentifier(byte[] encrypted, KeySet keys) {
         byte[] decrypted;
         try {
-            decrypted = AES.decryptAES(encrypted, keys.getInboundKey(), null, keys.getInboundCtr());
+            // Ensure input is sane
+            if (encrypted == null || encrypted.length <= IVBYTES) return null;
+            // Prepare IV
+            byte[] iv = new byte[IVBYTES + keys.getInboundCtr().length];
+            System.arraycopy(keys.getInboundCtr(), 0,  iv, 0,                           keys.getInboundCtr().length);
+            System.arraycopy(encrypted,            0,  iv, keys.getInboundCtr().length, IVBYTES);
+            // Prepare ciphertext to decrypt
+            byte[] ciphertext = new byte[encrypted.length - IVBYTES];
+            System.arraycopy(encrypted, IVBYTES, ciphertext, 0, encrypted.length - IVBYTES);
+            // Perform decryption
+            decrypted = AES.decryptAES(ciphertext, keys.getInboundKey(), null, iv);
         } catch (BadPaddingException e) {
             logger.severe("decryptKeysAndIdentifier: BadPaddingException");
             return null;
